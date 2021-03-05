7b11286ca95b9ff7136f7905ea73baade0a035bb
Kuismai <32294061+Kuismai@users.noreply.github.com>
2021-03-05 23:31:41 +0200

    Login form

--- Client/wi-client/App.js
+++ Client/wi-client/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Login from './login';
-import { StyleSheet, Text, View } from 'react-native';
+import Header from './header';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
       <Text>The very awesome store app</Text>
       <Login />
+      
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
--- Client/wi-client/login.js
+++ Client/wi-client/login.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 
-export default function Login() {
+export default function Login(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+    //simple login
+
+    function loginButton (event)
+    {
+            alert("It happened!");
+    }
+
+    //sign up
+
+    function registerButton (event)
+    {
+        alert("signing up...");
+    }
+
   return (
-        <div>Blah</div>
+      <View style={styles.container}>
+      <View style={styles.loginbox}>
+          <Text>Log In</Text>
+            <TextInput style={styles.TextInput}
+            defaultValue="username"
+            onChangeText={(username) => setUsername(username)}
+            />
+            <TextInput
+            secureTextEntry={true}
+            defaultValue="password"
+            onChangeText={(password) => setPassword(password)}
+            />
+            <TouchableOpacity 
+            onPress={loginButton}>
+                Log in</TouchableOpacity>
+        </View>
+        <View style={styles.loginbox}>
+        <Text>Register</Text>
+            <TextInput style={styles.TextInput}
+            defaultValue="username"
+            onChangeText={(username) => setUsername(username)}
+            />
+            <TextInput
+            secureTextEntry={true}
+            defaultValue="password"
+            onChangeText={(password) => setPassword(password)}
+            />
+            <TouchableOpacity 
+            onPress={registerButton}>Sign up</TouchableOpacity>
+        </View>
+        
+      </View>
   );
-}
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+    },
+    loginbox: {
+        flex: 1,
+        padding: 20,
+        margin: 15,
+    },
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+      },
+});
